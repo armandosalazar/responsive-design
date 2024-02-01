a7fed72034900b0b69d21a6860b57c64372ed3cc
Armando Salazar <armandosalazar351@gmail.com>
2024-02-01 00:37:56 -0600

    feat: add sidebar and found

--- js/main.js
+++ js/main.js
@@ -1,2 +1,24 @@
-window.addEventListener('resize', function () {
+var sidebar = document.getElementById('sidebar');
+if (sidebar) {
+    sidebar.style.display = 'none';
+}
+function openSideBard() {
+    sidebar && (sidebar.style.display = 'flex');
+}
+function closeSideBard() {
+    sidebar && (sidebar.style.display = 'none');
+}
+document.addEventListener('click', function (e) {
+    var _a;
+    var target = e.target;
+    if (target.classList.contains('sidebar__toggle')) {
+        console.log('sidebar::open');
+        openSideBard();
+    }
+    else if (!(((_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.id) === 'sidebar' || target.id === 'sidebar')) {
+        if ((sidebar === null || sidebar === void 0 ? void 0 : sidebar.style.display) == 'flex') {
+            console.log('sidebar::close');
+            closeSideBard();
+        }
+    }
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,2 +1,26 @@
-window.addEventListener('resize', function() {
-})
+const sidebar: HTMLElement | null = document.getElementById('sidebar');
+
+if (sidebar) {
+    sidebar.style.display = 'none';
+}
+
+function openSideBard(): void {
+    sidebar && (sidebar.style.display = 'flex');
+}
+
+function closeSideBard(): void {
+    sidebar && (sidebar.style.display = 'none');
+}
+
+document.addEventListener('click', (e: Event) => {
+    const target: HTMLElement = e.target as HTMLElement;
+    if (target.classList.contains('sidebar__toggle')) {
+        console.log('sidebar::open');
+        openSideBard();
+    } else if (!(target.parentElement?.id === 'sidebar' || target.id === 'sidebar')) {
+        if (sidebar?.style.display == 'flex') {
+            console.log('sidebar::close');
+            closeSideBard();
+        }
+    }
+});
